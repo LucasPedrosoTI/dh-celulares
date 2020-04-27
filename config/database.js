9e0662c5370534df5ac184f4fe6c507c689bfa06
lucaspedrosoti <lps1010@gmail.com>
2020-04-27 14:42:37 -0300

    corrigido database.js para trabalhar com variáveis de ambiente

--- config/database.js
+++ config/database.js
@@ -1,8 +1,11 @@
+require("dotenv").config();
+
 const config = {
-  username: "root",
-  password: "Brazil2014!",
-  database: "dh-celulares",
-  host: "localhost",
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: "mysql",
 };
 
