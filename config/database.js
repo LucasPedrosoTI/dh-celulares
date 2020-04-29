10b489d04e1025dab98ea56c12ca56c502dd8fd9
lucaspedrosoti <lps1010@gmail.com>
2020-04-28 22:12:29 -0300

    fix db

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 //mysql://bb4a029cba3986:f732445b@us-cdbr-iron-east-01.cleardb.net/heroku_bdef586a5063294?reconnect=true
 const config = {
-  username: bb4a029cba3986,
-  password: f732445b,
-  database: heroku_bdef586a5063294,
-  host: us - cdbr - iron - east - (01).cleardb.net,
+  username: "bb4a029cba3986",
+  password: "f732445b",
+  database: "heroku_bdef586a5063294",
+  host: "us-cdbr-iron-east-01.cleardb.net",
   //port: process.env.DB_PORT,
   dialect: "mysql",
 };
