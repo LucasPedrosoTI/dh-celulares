9fe017c8875c515223d3d83a6802e0cb63d7db70
lucaspedrosoti <lps1010@gmail.com>
2020-04-28 22:11:19 -0300

    heroku db

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
-
+//mysql://bb4a029cba3986:f732445b@us-cdbr-iron-east-01.cleardb.net/heroku_bdef586a5063294?reconnect=true
 const config = {
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  username: bb4a029cba3986,
+  password: f732445b,
+  database: heroku_bdef586a5063294,
+  host: us - cdbr - iron - east - (01).cleardb.net,
+  //port: process.env.DB_PORT,
   dialect: "mysql",
 };
 
